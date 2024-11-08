f8622de204d77875fee4d9acf06acdec3442c7ee
AnaLara-Silva <analara.silva444@gmail.com>
2024-11-08 17:00:42 -0300

    segunda atualização

--- js/play-js.js
+++ js/play-js.js
@@ -1,29 +1,80 @@
+let velocidade = localStorage.getItem("nivel") 
+
+let clicou = true;
+let score = 0
+let vidas = 3
+
 let body = document.querySelector("body")
  
 let img = document.createElement("img")
- 
 img.src="../img/mosquito.png"
 img.style.position = "absolute"
  
  
-function tamanho(){
-   let numero = Math.random() * 0.35;
-   img.style.transform = `scale(${numero+0.1})`
+function alteraTamanhoMosquito(){
+    //numero * (max - min) + min
+   let numeroAleatorio = Math.random() * 0.35;
+   img.style.transform = `scale(${numeroAleatorio + 0.1})`
 }
  
-function posicao(){
-    let y = Math.random() * (window.innerHeight *0.50)
-    let x = Math.random() * (window.innerWidth *0.60)
+function geraPosicao(){
+    let y = Math.random() * (window.innerHeight * 0.50)
+    let x = Math.random() * (window.innerWidth * 0.60)
+
     img.style.top = `${y}px`
     img.style.left = `${x}px`
 }
  
+img.addEventListener("click", function(){
+    clicou = true
+    img.remove()
+    ganharPonto()
+})
+
+function ganharPonto(){
+    score += 100
+    let domScore = document.getElementById("score-real")
+    domScore.innerText = score
+}
+
 setInterval(function(){
-    tamanho()
-    posicao()
+    alteraTamanhoMosquito()
+    geraPosicao()
+
+    body.appendChild(img)
+
+    if(!clicou){
+        
+        vidas--
+
+        if(vidas == 2){
+            let hat3 = document.getElementById("hat_3")
+            hat3.src = "img/coracao-vazio.png"
+        }
+
+        else if(vidas == 1){
+            let hat2 = document.getElementById("hat_2")
+            hat2.src = "img/coracao-vazio.png"
+        }
+
+        else if(vidas == 0){
+            let hat1 = document.getElementById("hat_1")
+            hat1.src = "img/coracao-vazio.png"
+
+            localStorage.setItem(
+                'score',
+                {
+                    lastScore: score
+                }
+            )
+
+            setTimeout(function(){
+                location.href = "gameover.html"
+            }, 1200)
+        }
+    }
+
+    clicou = false
  
-}, 2000)
+}, 500 * velocidade)
  
- 
-//O comando abaixo👇 adiciona um elemento no body que venha do html[h1,div,p]
-body.appendChild(img)
